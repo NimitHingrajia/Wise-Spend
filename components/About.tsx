7508687628aaa49ec089e8b8421bcce098c6c2ac
NimitHingrajia <nimit.2898@gmail.com>
2025-02-27 13:00:54 -0500

    Add About and Services components; update layout and links in RootLayout

---
+++ components/About.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+const About = () => {
+  return (
+    <div id="about">
+      <h4>About</h4>
+      <h1 className="text-4xl font-bold">Finding Story</h1>
+      <p className="text-lg mt-4">
+        A few years ago, when my parents moved to Canada, they struggled to
+        build their credit history despite being financially responsible. I saw
+        firsthand how confusing the system was and how it limited opportunities.
+        Realizing that many other adults faced the same challenge, I wanted to
+        create a solution. This web app simplifies credit building, offering
+        step-by-step guidance to help users take control of their financial
+        future.
+      </p>
+    </div>
+  );
+};
+
+export default About;
